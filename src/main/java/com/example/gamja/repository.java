b5c48df44f9134981f6350c8d497b6b0207ac1a7
Yeram9286 <126880644+Yeram9286@users.noreply.github.com>
2025-03-17 14:38:45 +0900

    Update repository.java

--- src/main/java/com/example/gamja/repository.java
+++ src/main/java/com/example/gamja/repository.java
@@ -3,7 +3,13 @@ package com.example.gamja;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
-//User에 대한 CRUD기능을 수행할수있도록 함
-public interface repository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email);
+
+public interface repository extends JpaRepository<User, Long> {//데이터베이스의 User테이블, 기본타입
+                                                                //즉 User와 관련된 CRUD기능 수행
+    Optional<User> findByEmail(String email); //이메일 기준으로 유저 조회
+    //Optional(User)-> 이메일 기준으로 조회했을때 데이터가 없을수도있기때문에
 }
+//엔티티 저장, ID로 엔티티조회, 모든 엔티티조회, 엔티티 삭제
+//ID로 엔티티 삭제, 총개수 반환, ID가 존재하는지확인
+//이 모든기능을 구현한 클래스를 자동생성해주기때문에 우리가 일일이 클래스를 구현할 필요가 없음
+//코드량이 줄고 유지보수가 쉬워짐
