626747bc09b678bbcc6b134e8c9461cf41d2f447
Yeram9286 <126880644+Yeram9286@users.noreply.github.com>
2025-03-17 14:35:07 +0900

    Update UserService.java

--- src/main/java/com/example/gamja/UserService.java
+++ src/main/java/com/example/gamja/UserService.java
@@ -5,8 +5,8 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
-@Service
-@Transactional
+@Service //서비스계층나타냄
+@Transactional//메서드 실행 중 예외가 발생하면 롤백되고,정상적으로 실행되면 커밋.데이터베이스 작업을 다룰 때 유용함
 public class UserService {  // 클래스명 변경
     private final repository userRepository;
 
@@ -20,6 +20,7 @@ public class UserService {  // 클래스명 변경
         Optional<User> existingUser = userRepository.findByEmail(userDto.getEmail()); //레포지토리에서이메일찾기
         if (existingUser.isPresent()) {//예외처리 -> existingUser에 위임(함수처럼 씀), 이미 있다면(없다면 엠티,널
             throw new IllegalArgumentException("이미 가입된 이메일입니다.");
+            //예외 발생시키는 코드 ->메서드중단 후 예외처리하는곳으로 전달, 잘못된인자나 부적절한 값이 전달 되었을때 발생
         }
 
         User user = new User();
