8dcdeeb37871c0b5cd1586bc5b7b2fdd01fa98e8
Yeram9286 <126880644+Yeram9286@users.noreply.github.com>
2025-03-17 14:37:39 +0900

    Update repository.java

--- src/main/java/com/example/gamja/repository.java
+++ src/main/java/com/example/gamja/repository.java
@@ -3,7 +3,7 @@ package com.example.gamja;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
-
+//User에 대한 CRUD기능을 수행할수있도록 함
 public interface repository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
 }
