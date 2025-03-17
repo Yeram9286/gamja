c63b51555c15e003d4071f065ab4f102342583bd
Yeram9286 <126880644+Yeram9286@users.noreply.github.com>
2025-03-17 14:18:11 +0900

    Update GamjaApplication.java

--- src/main/java/com/example/gamja/GamjaApplication.java
+++ src/main/java/com/example/gamja/GamjaApplication.java
@@ -3,11 +3,12 @@ package com.example.gamja;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication //Spring Boot설정 자동 구성
 public class GamjaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GamjaApplication.class, args);
+        //자동으로 Spring Boot가 웹서버와 애플리케이션 실행
     }
 
 }
